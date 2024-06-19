49667cc6959936a3b4e24da43cbc5987a25faffd
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-06-19 07:54:57 +0200

    Finish

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest;
+    let maxAge = 0;
+    
+    for(let i = 0; i < people.length; i++){
+        let currentAge;
+        if(Object.hasOwn(people[i], 'yearOfDeath')){
+            currentAge = people[i].yearOfDeath - people[i].yearOfBirth;
+        } else {
+            const date = new Date();
+            currentAge = date.getFullYear() - people[i].yearOfBirth;
+        }
+        
+        if(currentAge > maxAge){
+            oldest = people[i];
+            maxAge = currentAge;
+        }
+    }
+    
+    return oldest;
 };
 
 // Do not edit below this line
